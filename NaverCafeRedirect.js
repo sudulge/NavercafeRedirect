0cabed591cecafd32198d683a7d24e3064254352
Sudulge <57317487+sudulge@users.noreply.github.com>
2022-09-13 15:51:41 +0900

    Update NaverCafeRedirect.js

--- NaverCafeRedirect.js
+++ NaverCafeRedirect.js
@@ -1,17 +1,21 @@
 // ==UserScript==
 // @name        Naver Cafe Redirect from mobile to PC
-// @namespace   Violentmonkey Scripts
+// @namespace   https://github.com/sudulge/NavercafeRedirect
+// @updateURL   https://github.com/sudulge/NavercafeRedirect/raw/main/NaverCafeRedirect.js
+// @downloadURL https://github.com/sudulge/NavercafeRedirect/raw/main/NaverCafeRedirect.js
+// @author      https://github.com/sudulge/NavercafeRedirect
 // @match       *://m.cafe.naver.com/*
 // @grant       none
 // @version     1.0
-// @author      sudulge
 // @run-at      document-start
 // @description redirect from mobile url to PC url
 // ==/UserScript==
 
-let nowURL = window.location.pathname
-
-let cafeName = nowURL.split("/")[1]
-let articleNumber = nowURL.split("/")[2]
-
-window.location.href = `https://cafe.naver.com/${cafeName}/${articleNumber}`
+(function(){
+    let nowURL = window.location.pathname
+    
+    let cafeName = nowURL.split("/")[1]
+    let articleNumber = nowURL.split("/")[2]
+    
+    window.location.href = `https://cafe.naver.com/${cafeName}/${articleNumber}`
+})();
